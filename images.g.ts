3314a096d10a31c8c6d58f89957bb77feeeba886
JD10YL <155397262+JD10YL@users.noreply.github.com>
2024-02-23 12:36:37 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "C":return img`
+. . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . f f f f . . . . 
+. . . . . . . . . f d d d d f . . . 
+. . . . . . . . . f d d d d f . . . 
+. . . . . . f f f f d d d d f . . . 
+. . . . . f d d d d f d d d f . . . 
+. . . . . f d d d d f d d d f . . . 
+. . . . . f d d d d f d d d f . . . 
+. . . . f f d d d d f d d d f f f . 
+. . . f e f d d d d f d d d f e e f 
+. . f f e e f f f f f e f f e e f . 
+f f e e f e e e e e e e e e e e e f 
+e f f e e e e e e e e e e e f f f . 
+f e e e e e e e e e e e e e e e f . 
+. f f f f f f e e e e e f f f f f f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
